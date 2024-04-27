bb584cc0eea6ef15b2d9b691686835cc14972104
Dima-Korn <kornienkod1989@gmail.com>
2024-04-27 19:57:00 +0300

    Done acordion functions

--- src/js/about.js
+++ src/js/about.js
@@ -8,3 +8,15 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+new Accordion('.accordion-container-second', {
+    duration: 500,
+    showMultiple: true,
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const firstAccordionItem = document.getElementById('ac-0');
+    const firstAccordionContent = document.querySelector('.accordion-content.ac-panel');
+
+    firstAccordionItem.classList.add('is-active');
+    firstAccordionContent.style.height = 'auto';
+});
