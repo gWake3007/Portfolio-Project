5008c87a19bba53e13ef1fa096bb2ec04fa67090
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-04-29 00:29:17 +0200

    1

--- src/js/work-together-modal.js
+++ src/js/work-together-modal.js
@@ -3,10 +3,8 @@ import axios from 'axios';
 
 // import * as basicLightbox from 'basiclightbox';
 
-
 // document.getElementsById("footer").addEventListener("submit", async function (event) {
 //   event.preventDefault();
-
 
 //   const userEmail = document.querySelector(".footer-input[name='userEmail']").value;
 //   const userComments = document.getElementById("user-comments").value;
@@ -23,44 +21,39 @@ import axios from 'axios';
 //       // Показати модальне вікно з повідомленням про успішну відправку
 //       const successModal = basicLightbox.create("<h1>Success!</h1>");
 //       successModal.show();
-      
+
 //       // Очистити форму
 //       this.reset();
 //     } else {
-      
+
 //       alert("Error: " + response.statusText);
 //     }
 //   } catch (error) {
 //     console.error("Error:", error);
-    
+
 //     // Показати вспливаюче повідомлення про помилку
 //     alert("An error occurred. Please try again later.");
 //   }
 // });
 
-const form = document.getElementById("form");
+const form = document.getElementById('form');
 
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
 
-async function postRequests () {
-  return await axios.post('/requests');
+async function postRequests(email, comment) {
+  return await axios.post('/requests', { email: email, comment: comment });
 }
 
-async function getRequests () {
-  return await axios.get('/requests');
-}
 
-getRequests().then((result) => {
-  console.log(result);
-}).catch((err) => {
-  console.log(err);
-});;
+form.addEventListener('submit', submitForm);
 
-form.addEventListener("submit", submitForm);
-
-function submitForm (event) {
+function submitForm(event) {
   event.preventDefault();
   const form = event.target;
-  const {userEmail, userComments} = form.elements.value;
-  console.log(userEmail, userComments);
+  const { userEmail, userComments } = form.elements;
+  postRequests(userEmail.value, userComments.value)
+    .then((resp) =>  {
+      console.log(resp);
+    })
+    .catch(err => console.log(err));
 }
