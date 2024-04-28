eac6d333c178a0dabe614abb97553ae6687ef5f4
Dima-Korn <kornienkod1989@gmail.com>
2024-04-28 13:30:58 +0300

    done swiper js

--- src/js/about.js
+++ src/js/about.js
@@ -2,7 +2,7 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -21,25 +21,63 @@ document.addEventListener('DOMContentLoaded', function() {
     firstAccordionContent.style.height = 'auto';
 });
 
-const swiper = new Swiper('.swiper', {
-    // Default parameters
-    slidesPerView: 'auto',
-    spaceBetween: 0,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next', // Кнопка для переходу до наступного слайда
-      },
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 768px
-      768: {
-        slidesPerView: 'auto',
-        spaceBetween:0
-      },
-      // when window width is >= 1440px
-      1440: {
-        slidesPerView: 'auto',
-        spaceBetween: 0
-      }
-    }
-  });
+// Swiper features
+
+const swiper = new Swiper('.skills-container', {
+  modules: [Navigation, Keyboard, Mousewheel],
+  slidesPerView: 'auto',
+  loop: true,
+  speed: 700,
+  grabCursor: true,
+  allowTouchMove: true,
+  navigation: {
+    nextEl: '.skills-button-next',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  breakpoints: {
+    320: { slidesPerView: 2 },
+    768: { slidesPerView: 3 },
+    1440: { slidesPerView: 6 },
+  },
+  mousewheel: {
+    invert: false,
+  },
+  on: {
+    init: function () {
+      const activeIndex = this.activeIndex;
+      this.slides.forEach((slide, index) => {
+        if (index === activeIndex) {
+          slide.classList.add('active');
+        } else {
+          slide.classList.remove('active');
+        }
+      });
+    },
+    slideChange: function () {
+      const activeIndex = this.activeIndex;
+      this.slides.forEach((slide, index) => {
+        if (index === activeIndex) {
+          slide.classList.add('active');
+        } else {
+          slide.classList.remove('active');
+        }
+      });
+    },
+  },
+});
+
+const nextButton = document.querySelector('.skills-button-next');
+nextButton.addEventListener('click', function () {
+  const visibleSlides = swiper.slides.filter(slide => slide.isVisible);
+  const isEnd = swiper.isEnd && visibleSlides.length === swiper.slides.length;
+
+  if (isEnd || visibleSlides.length === swiper.slides.lengthVisible) {
+    swiper.slideTo(0);
+  } else {
+    swiper.slideNext();
+  }
+});
