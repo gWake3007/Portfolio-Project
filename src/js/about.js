6eeec2da23d32f86205043685d238c99a1545a91
Dima-Korn <kornienkod1989@gmail.com>
2024-04-28 18:50:36 +0300

    done swiper/ add hover on slider_acord btns

--- src/js/about.js
+++ src/js/about.js
@@ -5,8 +5,6 @@ import Swiper from 'swiper';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 new Accordion('.accordion-container-second', {
     duration: 500,
